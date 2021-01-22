02b18c3bdad1fec4a72e1eee62e2ad771ead6ff6
dmj1234 <1165689381@qq.com>
2021-01-22 16:33:43 +0800

    首页轮播图结束

--- miniprogram/pages/search/index.js
+++ miniprogram/pages/search/index.js
@@ -1,66 +1,46 @@
-// pages/search/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods: [],
+    isFocus: false,
+    inputValue: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 防抖
+  TimeId: 1,
+  handleInput(e) {
+    const { value } = e.detail;
+    // 检测合法性
+    if(!value.trim()) {
+      // 值不合法 为空值 重置数组和隐藏按钮
+      this.setData({
+        goods: [],
+        isFocus: false
+      })
+      return;
+    }
+    this.setData({
+      isFocus: true
+    })
+    // 发送请求
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value);
+    }, 1000);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取搜索数据
+  async qsearch(query) {
+    const goods = await request({url: '/goods/qsearch', data: {query}});
+    this.setData({
+      goods
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 取消按钮点击事件
+  handleCancle() {
+    this.setData({
+      inputValue: '',
+      goods: [],
+      isFocus: false
+    })
   }
 })
