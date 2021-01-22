854f2520795992c58f822b4e6840175ee5f9cc1c
dmj1234 <1165689381@qq.com>
2021-01-22 14:37:29 +0800

    初始化文档

--- miniprogram/pages/category/index.js
+++ miniprogram/pages/category/index.js
@@ -1,88 +1,66 @@
 // pages/category/index.js
-import regeneratorRuntime from '../../lib/runtime/runtime'
-import { request } from "../../request/index";
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        leftMenuList: [],
-        rightMenuList: [],
-        currentIndex: 0,
-        scrollTop:0
-    },
-    //接口返回数据
-    Cates: [],
+  /**
+   * 页面的初始数据
+   */
+  data: {
 
-    onLoad: function (options) {
-        // 小程序中： wx.setStorageSync("cates" ,{time:Date.now(),data:this.Cates});
-        // 先判断一下本地存储中有没有旧的数据
-        // 没有旧数据 直接发送新请求
-        // 有旧的数据同时旧的数据也没有过期  就使用本地存储中的旧数据即可
-      
-        //先获取本地存储中的数据（小程序也是存在本地存储）
-        const Cates = wx.getStorageSync("cates")
-        //开始判断
-        if(!Cates) {
-            //不存在   发送请求数据
-            this.getCates();
-        }else{
-            //有旧的数据  判断有没有过期   先定义一个  
-            if(Date.now() - Cates.time>1000*10){
-                this.getCates();
-            }else{
-                this.Cates = Cates.data;
-                let leftMenuList = this.Cates.map(v => v.cat_name);
-                let rightContent = this.Cates[0].children;
-                this.setData({
-                    leftMenuList,
-                    rightContent,
-                })
-            }
-        }
-    },
-   async getCates() {
-        // request({
-        //     url: "/categories"
-        // })
-        //     .then(res => {
-        //         this.Cates = res.data.message;
-        //         //把接口的数据存入本地的存储中
-        //         wx.setStorageSync("cates" ,{time:Date.now(),data:this.Cates});
-        //         //构造左侧大菜单数据
-        //         let leftMenuList = this.Cates.map(v => v.cat_name);
-        //         //构造右侧商品数据
-        //         let rightContent = this.Cates[0].children;
-        //         this.setData({
-        //             leftMenuList,
-        //             rightContent
-        //         })
-        //     })
-        const res=await request({url:"/categories"})
-        this.Cates = res;
-                //把接口的数据存入本地的存储中
-                wx.setStorageSync("cates" ,{time:Date.now(),data:this.Cates});
-                //构造左侧大菜单数据
-                let leftMenuList = this.Cates.map(v => v.cat_name);
-                //构造右侧商品数据
-                let rightContent = this.Cates[0].children;
-                this.setData({
-                    leftMenuList,
-                    rightContent
-                })
-    },
-    // 左侧带单的点击就的事件
-    // 给data中的currentIndex赋值就可以了
-    handleItemTap(e) {
-        const { index } = e.currentTarget.dataset;
-        let rightContent = this.Cates[index].children;
-        this.setData({
-            currentIndex: index,
-            rightContent,
-            scrollTop:0
-        })
+  },
 
-    }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,54 +1,118 @@
-// pages/index/index.js
-// 引入用来发送到请求的方法
-import { request} from "../../request/index";
+//index.js
+const app = getApp()
+
 Page({
-    data: {
-swiperList:[],
-//导航数组
-catesList:[],
-//楼层数据
-floorList:[],
-    },
-    onLoad:function(options){
-        //优化可以通过es6的promise来解决问题
-//         wx.request({
-//           url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems',
-//         // url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-// success:(result) => {
-//    this.setData({
-//        swiperList:result.data.message
-//    })
-// }
-//         });
-this.getSwiperList();
-this.getCateList();
-this.getFloorList();
-    },
-    //获取轮播图数据
-    getSwiperList(){
-        request({url: '/home/swiperdata'})
-        .then(result=> {
-            this.setData({
-                swiperList:result
-            })
-        })
-    },
-    //获取 分类导航数据
-    getCateList(){
-        request({url: '/home/catitems'})
-        .then(result=> {
-            this.setData({
-               catesList:result
-            })
-        })
-    },
-    // 获取楼层数据
-    getFloorList(){
-        request({url: '/home/floordata'})
-        .then(result=> {
-            this.setData({
-               floorList:result
-            })
-        })
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
     }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = `my-image${filePath.match(/\.[^.]+?$/)[0]}`
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
 })
