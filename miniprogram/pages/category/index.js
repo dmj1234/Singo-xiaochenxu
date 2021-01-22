417d8bdd387ce030102896b8084178e47579eaa4
dmj1234 <1165689381@qq.com>
2021-01-22 20:15:12 +0800

    实现分类页面 商品详情

--- miniprogram/pages/category/index.js
+++ miniprogram/pages/category/index.js
@@ -1,66 +1,52 @@
-// pages/category/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    leftMenuList: [],
+    rightContent: [],
+    currentIndex: 0,
+    scrollTop: 0
+  },
+  Cates: [],
+  onLoad(options) {
 
+    const Cates = wx.getStorageSync('cates');
+    if (!Cates) {
+      this.getCates();
+    } else {
+      if(Date.now() - Cates.time > 1000*60*5) {
+        this.getCates();
+      } else {
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(Cates => Cates.cat_name);
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  async getCates() {
+    const result = await request({ url: '/categories' });
+    this.Cates = result;
+    wx.setStorageSync('cates', { time: Date.now(), data: this.Cates });
+    let leftMenuList = this.Cates.map(Cates => Cates.cat_name);
+    let rightContent = this.Cates[0].children;
+    this.setData({
+      leftMenuList,
+      rightContent
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleItemTap(e) {
+    const { index } = e.currentTarget.dataset;
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent,
+      scrollTop: 0
+    })
   }
 })
--- miniprogram/pages/goods_list/index.js
+++ miniprogram/pages/goods_list/index.js
@@ -1,66 +1,94 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        title: '综合',
+        isActive: true
+      },
+      {
+        id: 1,
+        title: '销量',
+        isActive: false
+      },
+      {
+        id: 2,
+        title: '价格',
+        isActive: false
+      }
+    ],
+    goodsList: []
   },
+
+  // 请求数据
+  QueryParams: {
+    query: '',
+    cid: '',
+    pagenum: 1,
+    pagesize: 10
+  },
+
+  // 总页数
+  totalPages: 1,
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    this.QueryParams.cid = options.cid || '';
+    this.QueryParams.query = options.query || '';
+    this.getGoodsList();
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取商品列表数据
    */
-  onReady: function () {
-
+  async getGoodsList() {
+    const result = await request({url: '/goods/search', data: this.QueryParams});
+    this.totalPages = Math.ceil(result.total/this.QueryParams.pagesize);
+    this.setData({
+      goodsList: [...this.data.goodsList, ...result.goods]
+    })
+    // 关闭下拉刷新的窗口
+    wx.stopPullDownRefresh();
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 子向父转递数据
    */
-  onShow: function () {
-
+  handleItemChange(e) {
+    const { index } = e.detail;
+    let { tabs } = this.data;
+    tabs.forEach((item, i) => item.isActive = index === i ? true : false);
+    this.setData({
+      tabs
+    })
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 页面上滑事件
    */
-  onHide: function () {
-
+  onReachBottom() {
+    if(this.QueryParams.pagenum >= this.totalPages) {
+      wx.showToast({title: '没有数据了'});
+    } else {
+      this.QueryParams.pagenum ++;
+      this.getGoodsList();
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 下拉刷新
+  onPullDownRefresh() {
+    this.setData({
+      goodsList: []
+    })
+    this.QueryParams.pagenum = 1;
+    this.getGoodsList();
   }
 })
