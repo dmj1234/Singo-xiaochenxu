2c04fac726442e91ffa2a888483bab41bc3801bf
dmj1234 <1165689381@qq.com>
2021-01-23 22:57:00 +0800

    个人中心界面完成

--- miniprogram/pages/goods_detail/index.js
+++ miniprogram/pages/goods_detail/index.js
@@ -2,18 +2,11 @@ import { request } from "../../request/index.js";
 import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     goodsDetail: {},
-    // 商品是否被收藏过
     isCollect: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onShow() {
     let pages =  getCurrentPages();
     let currentPages = pages[pages.length-1];
@@ -21,32 +14,24 @@ Page({
     this.getGoodsDetail(goods_id);
   },
 
-  // 商品详情数据
   GoodsDetail: {},
 
-  /**
-   * 获取商品详情数据
-   */
   async getGoodsDetail(id) {
     const result = await request({url: '/goods/detail?goods_id=' + id});
     this.GoodsDetail = result;
-    // 获取缓存中的商品收藏的数据
     let collect = wx.getStorageSync("collect") || [];
-    // 判断当前商品是否被收藏
     let isCollect = collect.some(v => v.goods_id === this.GoodsDetail.goods_id);
     this.setData({
       goodsDetail: {
         pics            : result.pics,
         goods_price     : result.goods_price,
         goods_name      : result.goods_name,
-        // 临时把 .webp 文件名改为 .jpg 做部分机型兼容处理
         goods_introduce : result.goods_introduce.replace(/\.webp/g, '.jpg')
       },
       isCollect
     })
   },
 
-  // 点击轮播图放大预览
   handlePrevewImage(e) {
     const urls = this.GoodsDetail.pics.map(v => v.pics_mid);
     const current = e.currentTarget.dataset.url;
@@ -56,38 +41,29 @@ Page({
     });
   },
 
-  // 点击加入购物车
   handleCartAdd() {
     let cart = wx.getStorageSync('cart') || [];
-    // 判断商品对象是否存入在购物车数组中
     let index = cart.findIndex(v => v.goods_id === this.GoodsDetail.goods_id);
     if(index === -1) {
-      // 不存在 第一次加入
       this.GoodsDetail.num = 1;
       this.GoodsDetail.checked = true;
       cart.push(this.GoodsDetail);
     } else {
-      // 已存在 数量++
       cart[index].num ++;
     }
     wx.setStorageSync('cart', cart);
     wx.showToast({
       title: '添加成功',
       icon: 'success',
-      mask: true  // 防止用户手抖
+      mask: true  
     })
   },
 
-  // 点击商品收藏
   handleCollect() {
     let isCollect = false;
-    // 获取缓存中的商品收藏数组
     let collect = wx.getStorageSync("collect") || [];
-    // 判断该商品是否被收藏过
     let index = collect.findIndex(v => v.goods_id === this.GoodsDetail.goods_id);
-    // 当 index!=-1 表示 已经收藏过
     if(index !== -1) {
-      // 已经收藏，在数组中删除该商品
       collect.splice(index, 1);
       isCollect = false;
       wx.showToast({
@@ -96,7 +72,6 @@ Page({
         mask: true,
       });
     }else{
-      // 没有收藏过
       collect.push(this.GoodsDetail);
       isCollect = true;
       wx.showToast({
@@ -105,9 +80,7 @@ Page({
         mask: true,
       });
     }
-    // 把数组存入到缓存中
     wx.setStorageSync("collect", collect);
-    // 修改data
     this.setData({
       isCollect
     })
--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -1,66 +1,63 @@
-// pages/order/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    orders: [],
+    tabs: [
+      {
+        id: 0,
+        title: '全部订单',
+        isActive: true
+      },
+      {
+        id: 1,
+        title: '待付款',
+        isActive: false
+      },
+      {
+        id: 2,
+        title: '待收货',
+        isActive: false
+      },
+      {
+        id: 3,
+        title: '退款/退货',
+        isActive: false
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+//   onShow() {
+//     const token = wx.getStorageSync("token");
+//     if(!token) {
+//       wx.navigateTo({
+//         url: '/pages/auth/index'
+//       });
+//       return;
+//     }
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+//     let pages = getCurrentPages();
+//     let currentPage = pages[pages.length-1];
+//     const { type } = currentPage.options;
+//     this.changeTitleByIndex(type-1);
+//     this.getOrderList(type);
+//   },
+//   async getOrderList(type) {
+//     const res = await request({url: '/my/orders/all', data: {type}});
+//     this.setData({
+//       orders: res.orders.map(v => ({...v, creat_time_cn: (new Date(v.create_time*1000).toLocaleString())}))
+//     })
+//   },
+//   changeTitleByIndex(index) {
+//     let { tabs } = this.data;
+//     tabs.forEach((item, i) => item.isActive = index === i ? true : false);
+//     this.setData({
+//       tabs
+//     })
+//   },
+//   handleItemChange(e) {
+//     const { index } = e.detail;
+//     changeTitleByIndex(index);
+//     this.getOrderList(index+1);
+//   }
 })
